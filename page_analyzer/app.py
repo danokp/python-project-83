db4f08b7d2935af6a064e85bf0ebd2f88ed84989
Daniil <danokp@mail.ru>
2023-04-05 12:54:06 +0700

    Change: import config before import database in app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 from flask import Flask, flash, redirect, render_template, request, url_for
 from validators.url import url as is_valid_url
 
+load_dotenv()
 from .database import UrlChecks, Urls
 from .utils import normalize_url, scrap_web_page
 
 
-load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
