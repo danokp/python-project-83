135d5c4db2551ace461e9cb7c06b2d04b4df8055
Daniil <danokp@mail.ru>
2023-04-25 00:57:59 +0700

    Delete removing of "www." from url while normalizing

--- page_analyzer/utils.py
+++ page_analyzer/utils.py
@@ -8,8 +8,6 @@ def normalize_url(url):
         urlsplit(url).scheme.lower(),
         urlsplit(url).netloc.lower(),
     )
-    if netloc.startswith('www.'):
-        netloc = netloc[4:]
     return urlunsplit((scheme, netloc, '', '', ''))
 
 
